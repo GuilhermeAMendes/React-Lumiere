3a3ad72006c15ce2df960b2dd8397274ef54d8ec
GuiAMendes <guilherme.andmendes03@gmail.com>
2024-07-03 18:56:24 -0300

    feat: code conversion in the FAQ component to array used map to create dynamic elements

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import "./Main.css";
 
 function App() {
   const [page] = useState("Início");
-  const [anchor_1] = useState('Usuários');
-  const [anchor_2] = useState('Livros');
+  const [anchor_1] = useState("Usuários");
+  const [anchor_2] = useState("Livros");
   const [cite, setCite] = useState("");
   const [author, setAuthor] = useState("");
+  const [quests] = useState([
+    {
+      textQuest: "Como posso fazer meu cadastro?",
+      textResponse: "Você pode usar a barra de pesquisa no topo da página ou navegar pelas categorias disponíveis na seção de livros.",
+    },
+    {
+      textQuest: "Como faço para atualizar meus dados cadastrais?",
+      textResponse: "Para atualizar seus dados de cadastro, vá à sua conta utilizando a seção Clientes, Adicione o seu identificador e informações desejadas para atualização e após o procedimento clique em atualizar dados.",
+    },
+    {
+      textQuest: "Como posso encontrar um livro específico?",
+      textResponse: "Você pode usar a barra de pesquisa no topo da página ou navegar pelas categorias disponíveis na seção de livros.",
+    }
+  ]);
 
   useEffect(() => {
     async function getCite() {
@@ -16,7 +30,9 @@ function App() {
         let data = await res.json();
         let randomIndex = Math.floor(Math.random() * data.length);
         setCite(data[randomIndex].text);
-        setAuthor(data[randomIndex].author ? data[randomIndex].author : "Desconhecido");
+        setAuthor(
+          data[randomIndex].author ? data[randomIndex].author : "Desconhecido"
+        );
       } catch (e) {
         console.log(e);
         console.log(e.name);
@@ -30,7 +46,7 @@ function App() {
     <div>
       <Header current_page={page} anchor_1={anchor_1} anchor_2={anchor_2} />
       <Main quote={cite} author={author} />
-      <Faq />
+      <Faq faqs={quests}/>
       <Footer />
     </div>
   );
--- src/Components/Main/Faq.js
+++ src/Components/Main/Faq.js
@@ -1,31 +1,15 @@
 import React from "react";
 
-export const Faq = () => {
+export const Faq = ({ faqs }) => {
   return (
     <div id="faq">
       <h2>Perguntas Frequentes</h2>
-      <details>
-        <summary>Como posso fazer meu cadastro?</summary>
-        <p>
-          Você pode usar a barra de pesquisa no topo da página ou navegar pelas
-          categorias disponíveis na seção de livros.
-        </p>
-      </details>
-      <details>
-        <summary>Como faço para atualizar meus dados cadastrais?</summary>
-        <p>
-          Para atualizar seus dados de cadastro, vá à sua conta utilizando a
-          seção Clientes, Adicione o seu identificador e informações desejadas
-          para atualização e após o procedimento clique em atualizar dados.
-        </p>
-      </details>
-      <details>
-        <summary>Como posso encontrar um livro específico?</summary>
-        <p>
-          Você pode usar a barra de pesquisa no topo da página ou navegar pelas
-          categorias disponíveis na seção de livros.
-        </p>
-      </details>
+      {faqs.map((item, index) => (
+        <details key={index}>
+          <summary>{item.textQuest}</summary>
+          <p>{item.textResponse}</p>
+        </details>
+      ))}
     </div>
   );
 };
