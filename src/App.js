c0702829206726c341150d3190ac623c71fa49b8
GuiAMendes <guilherme.andmendes03@gmail.com>
2024-07-04 12:01:19 -0300

    feat: update project color palette

--- src/App.js
+++ src/App.js
@@ -11,16 +11,19 @@ function App() {
   const [quests] = useState([
     {
       textQuest: "Como posso fazer meu cadastro?",
-      textResponse: "Você pode usar a barra de pesquisa no topo da página ou navegar pelas categorias disponíveis na seção de livros.",
+      textResponse:
+        "Você pode usar a barra de pesquisa no topo da página ou navegar pelas categorias disponíveis na seção de livros.",
     },
     {
       textQuest: "Como faço para atualizar meus dados cadastrais?",
-      textResponse: "Para atualizar seus dados de cadastro, vá à sua conta utilizando a seção Clientes, Adicione o seu identificador e informações desejadas para atualização e após o procedimento clique em atualizar dados.",
+      textResponse:
+        "Para atualizar seus dados de cadastro, vá à sua conta utilizando a seção Clientes, Adicione o seu identificador e informações desejadas para atualização e após o procedimento clique em atualizar dados.",
     },
     {
       textQuest: "Como posso encontrar um livro específico?",
-      textResponse: "Você pode usar a barra de pesquisa no topo da página ou navegar pelas categorias disponíveis na seção de livros.",
-    }
+      textResponse:
+        "Você pode usar a barra de pesquisa no topo da página ou navegar pelas categorias disponíveis na seção de livros.",
+    },
   ]);
 
   useEffect(() => {
@@ -28,11 +31,9 @@ function App() {
       try {
         const res = await fetch("https://type.fit/api/quotes");
         let data = await res.json();
-        let randomIndex = Math.floor(Math.random() * data.length);
-        setCite(data[randomIndex].text);
-        setAuthor(
-          data[randomIndex].author ? data[randomIndex].author : "Desconhecido"
-        );
+        let index = Math.floor(Math.random() * data.length);
+        setCite(data[index].text);
+        setAuthor(data[index].author ? data[index].author : "Desconhecido");
       } catch (e) {
         console.log(e);
         console.log(e.name);
@@ -46,7 +47,7 @@ function App() {
     <div>
       <Header current_page={page} anchor_1={anchor_1} anchor_2={anchor_2} />
       <Main quote={cite} author={author} />
-      <Faq faqs={quests}/>
+      <Faq faqs={quests} />
       <Footer />
     </div>
   );
